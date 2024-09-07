ab8266db0b70e2728bb11dc2dfd5d902b26be078
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-07 12:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function allGoodToGo() {
+  let form = document.querySelector("#form_element");
+  let emailValid = checkEmail();
+  let passwordValid = checkPassword();
+
+  if (emailValid && passwordValid) {
+    let allGoodPara = document.createElement("p");
+    allGoodPara.classList.add("correct");
+    allGoodPara.innerText = `All good to go!`;
+
+    form.appendChild(allGoodPara);
+  }
+}
+function checkEmail() {
+  let inputValue = document.getElementById("email").value;
+  let errorPara = document.querySelector("#para1");
+
+  if (
+    inputValue.length > 2 &&
+    inputValue.includes("@") &&
+    inputValue.includes(".")
+  ) {
+    errorPara.style.display = `none`;
+    return true;
+  } else {
+    errorPara.style.display = `block`;
+    return false;
+  }
+}
+function checkPassword() {
+  let inputValue2 = document.getElementById("password").value;
+  let errorPara2 = document.getElementById("para2");
+
+  if (inputValue2.length > 7) {
+    errorPara2.style.display = `none`;
+    return true;
+  } else {
+    errorPara2.style.display = `block`;
+    return false;
+  }
+}
+function confirmWindow() {
+  let confirm = window.confirm("Are you sure you want to submit?");
+
+  if (confirm === true) {
+    alert("successful signup!");
+  } else {
+    location.reload();
+  }
+}
